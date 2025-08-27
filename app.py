18f5ff4c0be64524f109acdbc46080584b39d0e6
Smrii12 <154430299+Smrii12@users.noreply.github.com>
2025-08-27 16:13:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -707,4 +707,7 @@ def predict_progress():
 if __name__ == '__main__':
     init_db()
     insert_default_videos()
-    app.run(debug=True, port=5000)
+     port = int(os.environ.get("PORT", 5000))  
+     app.run(host="0.0.0.0", port=port, debug=True)
+    # app.run(debug=True, port=5000)
+
